1c5855afcd4cb6acbd316b1f1ece1fde2f00abe3
odinhufnagl <odin.hufnagl@gmail.com>
2023-08-28 12:29:50 +0200

    changes

---
+++ build/src/routes/api/functions/blockUser.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const blockUser_1 = __importDefault(require("../../../controllers/api/functions/blockUser"));
+const wrappers_1 = require("../../../wrappers");
+const router = express_1.default.Router();
+router.post("/", (0, wrappers_1.asyncWrapper)(blockUser_1.default.blockUserController));
+exports.default = router;
