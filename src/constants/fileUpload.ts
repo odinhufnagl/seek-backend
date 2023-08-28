e49cda6d9e413558b5bac5aed7cb723d0d9a7a8f
odinhufnagl <odin.hufnagl@gmail.com>
2023-08-28 12:27:30 +0200

    resize image

--- src/constants/fileUpload.ts
+++ src/constants/fileUpload.ts
@@ -1,5 +1,12 @@
 export class ExternalStorageConstants {
   public static defaultUploadFolderPath = {
     SERVER_UPLOADS: "serverUploads",
+    PROFILE_IMAGES: "profileImages",
+  };
+  public static defaultQuality = {
+    PROFILE_IMAGE: 80,
+  };
+  public static defaultSize = {
+    PROFILE_IMAGE: { width: 800, height: null },
   };
 }
