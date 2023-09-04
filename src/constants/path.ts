92ca8e6e3b5a69600febcbe2fc6e56476fe25ab4
odinhufnagl <odin.hufnagl@gmail.com>
2023-09-04 12:01:33 +0200

    reset password

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,10 +1,10 @@
 import path from "path";
 
 const { dirname } = require("path");
-//const appDir = dirname(require.main?.filename);
-
+const appDir = dirname(require.main?.filename);
 const defaultFolderPath = {
-  TEMP_FILE_FOLDER: path.join(__dirname, "assets/temp"),
+  TEMP_FILE_FOLDER: path.join(appDir, "assets/temp"),
+  EMAIL_TEMPLATES_FOLDER: path.join(appDir, "assets/emailTemplates"),
 } as const;
 
 export class PathConstants {
